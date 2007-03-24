d420fbcedc0317825f0f08d7f31afab76e5e9bcc
Mischa Peters <mischapeters@users.noreply.github.com>
2007-03-24 07:27:00 +0000

    Initial Import in SourceForge

--- admin/sendmail.php
+++ admin/sendmail.php
@@ -1,37 +1,33 @@
 <?php
-require "config.inc.php";
-require "my_lib.php";
+require "../config.inc.php";
+require "../my_lib.php";
 
-$sessid = check_session();
-$sessid_domain = $sessid['domain'];
-$sessid_username = $sessid['username'];
-
-print_header("menu");
+print_header("admin");
 
 if (!empty($_POST['submit'])) {
 	$to = $_POST['to'];
 
 	if (empty($to)) print_error("You need to select an email address.");
 
-	$headers = "From: $sessid_username";
+	$headers = "From: $admin_email";
 	$subject = "Welcome";
 	$message = "Hi,\n\nWelcome to your new email account.\n\n";
 	if (!mail($to, $subject, $message, $headers)) {
 		print_error("Unable to send message to $to!");
 	}
 	print "Successfully send email to $to\n";
-	print "<p>\n";
+	print "<p />\n";
 }
 ?>
 Send test message to a new mailbox.
-<p>
+<p />
 <form name="mailbox" method="post">
 <table class="form">
-<tr><td>From:</td><td><?php print "$sessid_username"; ?></td></tr>
+<tr><td>From:</td><td><?php print "$admin_email"; ?></td></tr>
 <tr><td>To:</td><td>
 <select name="to">
 <?php
-$result = db_query("SELECT username FROM mailbox WHERE domain='$sessid_domain' ORDER BY domain");
+$result = db_query("SELECT username FROM mailbox ORDER BY domain");
 while ($row = mysql_fetch_array ($result['result'])) {
 	print "<option>" . $row['username'] . "</option>";
 }
